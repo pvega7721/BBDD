018843319a6f2ba041adb14243a94fc1b8ea77dd
pvega7721 <pvega7721@ceuandalucia.es>
2024-04-05 23:01:06 +0200

    .

---
+++ Tercer trimestre/Repaso.sql
@@ -0,0 +1,19 @@
+--Ejercicio1
+declare
+    fecha date;
+    fecha1 date := '06-04-2021';
+    numero1 number(3);
+    numero2 number(4,2) := 10.99;
+    caracter varchar2(10) := 'DAM/DAW';
+    numero3 constant number := 11;
+begin
+    dbms_output.put_line(numero2);
+end;
+/
+--Ejercicio2
+declare
+
+begin
+
+end;
+/
