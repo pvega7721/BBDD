0423bb315d07455bde0c1f90ea83c6e302f53d17
pvega7721 <pvega7721@ceuandalucia.es>
2024-04-22 11:26:14 +0200

    .

--- Tercer trimestre/Repaso.sql
+++ Tercer trimestre/Repaso.sql
@@ -1,3 +1,4 @@
+set serveroutput on;
 --Ejercicio1
 declare
     fecha date;
@@ -10,10 +11,48 @@ begin
     dbms_output.put_line(numero2);
 end;
 /
+
 --Ejercicio2
 declare
-
+    variable1 emp.ename%type;
+    variable2 emp%rowtype;
 begin
+    dbms_output.put_line(variable1);
+    --Variable2 no puede imprimirse sola, al ser una fila entera hay que especificar que columna se quiere imprimir
+    dbms_output.put_line(variable2.deptno);
+end;
+/
 
+--Ejercicio3
+declare
+    variable1 emp.ename%type;
+    variable2 emp%rowtype;
+begin
+    select ename into variable1 from emp where empno = 7839;
+    dbms_output.put_line(variable1);
+    select * into variable2 from emp where empno = 7698;
+    dbms_output.put_line(variable2.deptno);
+end;
+/
+
+--Ejercicio5
+declare
+    base integer := &base_triangulo;
+    altura integer := &altura_triangulo;
+    area integer;
+begin
+    area := (base*altura) /2;
+    dbms_output.put_line('El área del triángulo es: ' || area);
+end;
+/
+
+--Ejercicio 6
+declare
+    nombre varchar2(50);
+    apellido varchar2(50);
+begin
+    nombre := &nombre;
+    apellido := &apellido;
+    dbms_output.put_line('Hola ' || nombre || ' ' || apellido);
 end;
 /
