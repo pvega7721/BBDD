d3bb80d9ebd8fe91a23d6095379e5477d43bcb9a
pvega7721 <pvega7721@ceuandalucia.es>
2024-04-29 11:02:32 +0200

    .

--- Tercer trimestre/Repaso.sql
+++ Tercer trimestre/Repaso.sql
@@ -8,6 +8,7 @@ declare
     caracter varchar2(10) := 'DAM/DAW';
     numero3 constant number := 11;
 begin
+    dbms_output.put_line('Ejercicio1');
     dbms_output.put_line(numero2);
 end;
 /
@@ -17,6 +18,7 @@ declare
     variable1 emp.ename%type;
     variable2 emp%rowtype;
 begin
+    dbms_output.put_line('Ejercicio2');
     dbms_output.put_line(variable1);
     --Variable2 no puede imprimirse sola, al ser una fila entera hay que especificar que columna se quiere imprimir
     dbms_output.put_line(variable2.deptno);
@@ -28,6 +30,7 @@ declare
     variable1 emp.ename%type;
     variable2 emp%rowtype;
 begin
+    dbms_output.put_line('Ejercicio3');
     select ename into variable1 from emp where empno = 7839;
     dbms_output.put_line(variable1);
     select * into variable2 from emp where empno = 7698;
@@ -41,6 +44,7 @@ declare
     altura integer := &altura_triangulo;
     area integer;
 begin
+    dbms_output.put_line('Ejercicio5');
     area := (base*altura) /2;
     dbms_output.put_line('El área del triángulo es: ' || area);
 end;
@@ -51,8 +55,95 @@ declare
     nombre varchar2(50);
     apellido varchar2(50);
 begin
-    nombre := &nombre;
-    apellido := &apellido;
+    dbms_output.put_line('Ejercicio6');
+    nombre := '&nombre';
+    apellido := '&apellido';
     dbms_output.put_line('Hola ' || nombre || ' ' || apellido);
+end;
+/
+
+--Ejercicio7
+declare
+num1 integer :=8;
+num2 integer :=4;
+begin
+    dbms_output.put_line('Ejercicio7');
+    dbms_output.put_line('Suma');
+        dbms_output.put_line(num1+num2);
+    dbms_output.put_line('Resta');
+        dbms_output.put_line(num1-num2);
+    dbms_output.put_line('Multiplicación');
+        dbms_output.put_line(num1*num2);
+    dbms_output.put_line('División');
+        dbms_output.put_line(num1/num2);
+end;
+/
+
+--Ejercicio8
+declare
+    num1 integer := 7;
+    num2 integer := 3;
+begin
+    dbms_output.put_line('Ejercicio8');
+    if num1 > num2 then
+        dbms_output.put_line(num1+num2);
+    end if;
+end;
+/
+
+--Ejercicio9
+declare
+    num1 integer := 3;
+    num2 integer := 7;
+begin
+    dbms_output.put_line('Ejercicio9');
+    if num1 > num2 then
+        dbms_output.put_line(num1-num2);
+    ELSE
+        dbms_output.put_line(num1 || ' es mayor que ' || num2);
+    end if;
+end;
+/
+
+--Ejercicio 10
+declare
+    nota integer := '&Introduce_un_número';
+begin
+    dbms_output.put_line('Ejercicio10');
+    if nota <5 then
+        dbms_output.put_line('Suspenso');
+    elsif nota <6 then
+        dbms_output.put_line('Aprobado');
+    elsif nota <7 then
+        dbms_output.put_line('Bien');
+    elsif nota <9 then
+        dbms_output.put_line('Notable');
+    elsif nota <11 then
+        dbms_output.put_line('Sobresaliente');
+    else
+        dbms_output.put_line('Nota no válida');
+    end if;
+end;
+/
+
+--Ejercicio 11
+declare
+    nota integer := '&Introduce_un_número';
+begin
+    dbms_output.put_line('Ejercicio10');
+    case nota
+    when nota <5 then
+        dbms_output.put_line('Suspenso');
+    when nota <6 then
+        dbms_output.put_line('Aprobado');
+    when nota <7 then
+        dbms_output.put_line('Bien');
+    when nota <9 then
+        dbms_output.put_line('Notable');
+    when nota <11 then
+        dbms_output.put_line('Sobresaliente');
+    else
+        dbms_output.put_line('Nota no válida');
+    end case;
 end;
 /
